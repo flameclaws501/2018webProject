33b8687e96d9080cb7e02a9e009b871b356da8e0
flameclaws501 <willy870501@gmail.com>
2018-12-18 19:11:06 +0800

    fix main png
    fix main png problem

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
             output += "<div class = 'col-sm-4'>" + 
                       "<h4>" + data12[idx].Chinese + "</h4>" +
                       "<h5>" + data12[idx].English + "</h5><br>" +
-                      "<a href = '" + data12[idx].Name + ".html'><img src = 'Image/main/" + data12[idx].Name + ".png' alt = 'pagePicture' id = 'pic' align = 'middle'></a>" + 
+                      "<a href = '" + data12[idx].Name + ".html'><img src = 'Image/main/" + data12[idx].Name + ".PNG' alt = 'pagePicture' id = 'pic' align = 'middle'></a>" + 
                       "</div>";
             
             if(idx % 3 == 2)
